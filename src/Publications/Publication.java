64d4b6bfb9767978cf41434a00eb6b1929a4ac06
yuriclaure <yuriclaure@gmail.com>
2013-03-09 16:33:39 -0400

    Publication

--- src/Publications/Publication.java
+++ src/Publications/Publication.java
@@ -1,6 +1,13 @@
 package Publications;
 
+import java.io.*;
+
 public abstract class Publication {
+	
+	public abstract void printXML(OutputStream out);
+	public abstract void print();
+	public abstract void 
+	
 	
 	
 
